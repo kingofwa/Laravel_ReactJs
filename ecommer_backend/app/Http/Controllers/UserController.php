d8d009170fcb70a255a247eae1192ae2f841fa0e
nv_leRikaiTech <vanle.nguyen@rikai.technology>
2022-07-19 14:47:25 +0700

    config

--- ecommer_backend/app/Http/Controllers/UserController.php
+++ ecommer_backend/app/Http/Controllers/UserController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     //
-    function register()
+    function register(Request $request)
     {
-        return "c";
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->firstname = $request->input('firstname');
+        $user->lastname = $request->input('lastname');
+        $user->save();
+        return $user;
     }
+    function login(Request $res)
+    {
+        $user = User::where('email', $res->email)->first();
+        if (!$user ||!Hash::check($res->password, $user->password)) {
+            return ["error"=> "Email or password is not matched"];
+        } else {
+            return $user;
+        }
+    }
+
+
 }
